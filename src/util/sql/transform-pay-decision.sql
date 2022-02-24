125b22bf99898805af990ee2fcb52a05f5562ee4
apeltola <antti.peltola@gofore.com>
2022-02-24 16:17:16 +0200

    Fix daterange comparison for pay decision transform

--- src/util/sql/transform-pay-decision.sql
+++ src/util/sql/transform-pay-decision.sql
@@ -187,9 +187,9 @@ JOIN ${migrationSchema:name}.paydecisions pd ON pd.internaldecisionnumber = pdr.
 JOIN ${migrationSchema:name}.evaka_fee_decision efd ON efd.effica_internal_decision_number = pd.internaldecisionnumber
 LEFT JOIN ${migrationSchema:name}.evaka_person child ON child.effica_ssn = pdr.person
 LEFT JOIN ${migrationSchema:name}.evaka_placement ep ON ep.child_id = child.id
-    AND daterange(ep.start_date, ep.end_date) @> efd.start_date
+    AND daterange(ep.start_date, ep.end_date, '[]') @> efd.start_date
 LEFT JOIN ${migrationSchema:name}.evaka_service_need esn ON esn.placement_id = ep.id
-    AND daterange(esn.start_date, esn.end_date) @> efd.start_date
+    AND daterange(esn.start_date, esn.end_date, '[]') @> efd.start_date
 WHERE pdr.rowtype = 5;
 
 DROP TABLE IF EXISTS ${migrationSchema:name}.evaka_fee_decision_child_todo;
