4517bac876112eefc99f273a952754ffe329dd09
Jarkko Pätäri <jarkko.patari@gofore.com>
2024-04-09 19:26:51 +0300

    Add fixes from first sem data migration

--- src/util/sql/transfer/placement.sql
+++ src/util/sql/transfer/placement.sql
@@ -34,3 +34,15 @@ SELECT group_placement_id,
        placement_start_date,
        placement_end_date
 FROM $(migrationSchema:name).evaka_placement_valid;
+
+-- fix missing service needs
+WITH extended_service_needs AS (SELECT service_need.*,
+                                       daterange(placement.start_date, placement.end_date, '[]') -
+                                       daterange(service_need.start_date, service_need.end_date, '[]') AS range
+                                FROM placement
+                                         JOIN service_need ON service_need.placement_id = placement.id)
+INSERT
+INTO service_need (option_id, placement_id, start_date, end_date, confirmed_by, confirmed_at, shift_care)
+SELECT option_id, placement_id, lower(range), upper(range) - interval '1 day', confirmed_by, confirmed_at, shift_care
+FROM extended_service_needs
+WHERE NOT isempty(range);
